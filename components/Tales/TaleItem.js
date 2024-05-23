bc81599498bf8b3c3fac0306cd9a2b8bda754830
yeryshevArtem <artem.yeryshev@gmail.com>
2024-05-23 21:53:07 +0200

    Add image in TaleItem

--- components/Tales/TaleItem.js
+++ components/Tales/TaleItem.js
@@ -1,9 +1,9 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 function TaleItem({ title }) {
     return (
         <View style={styles.container}>
-            <View style={styles.imageBox}></View>
+            <Image source={require('../../assets/Hansel-and-gretel-rackham.jpg')} style={styles.image} />
             <View style={styles.titleBox}>
                 <Text>{title}</Text>
             </View>
@@ -15,16 +15,16 @@ export default TaleItem;
 
 const styles = StyleSheet.create({
     container: {
-        marginHorizontal: 10,
-        marginVertical: 10
-    },
-    imageBox: {
-        height: 150,
         width: 150,
-        borderRadius: 10,
-        borderWidth: 2,
+        marginHorizontal: 10,
+        marginVertical: 10,
     },
     titleBox: {
         marginVertical: 10
+    },
+    image: {
+        width: 150,
+        height: 150,
+        borderRadius: 10
     }
 });

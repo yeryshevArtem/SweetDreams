edd0a02487630c5a4f3afae33324e8bfca45f18e
yeryshevArtem <artem.yeryshev@gmail.com>
2024-06-19 23:21:09 +0200

    Change background image, refactor ErrorAlert

--- components/ui/ErrorAlert.js
+++ components/ui/ErrorAlert.js
@@ -5,12 +5,17 @@ import { GlobalStyles } from '../../constants/styles';
 // constants 
 import { templates } from '../../constants/templates';
 
-function Error({ message = templates.errorAlertDefaultBody, onConfirm, btnConfirmTitle = templates.errorAlertDefaultButtonText }) {
+function Error({ 
+    message = templates.errorAlertDefaultBody, 
+    onConfirm, 
+    btnConfirmTitle = templates.errorAlertDefaultButtonText,
+    title = templates.errorAlertDefaultTitle
+}) {
     return (
         <View style={styles.container}>
             <View style={styles.errorBox}>
-                <Text style={styles.errorText}>{templates.errorAlertDefaultTitle}</Text>
-                <Text style={styles.errorText}>{message}</Text>
+                <Text style={styles.errorTitle}>{title}</Text>
+                <Text style={styles.errorBody}>{message}</Text>
                 {
                     onConfirm && (
                         <View style={styles.buttonBox}>
@@ -27,20 +32,27 @@ export default Error;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+
     },
     errorBox: {
         borderRadius: 10,
         paddingHorizontal: 10,
         paddingVertical: 10,
-        backgroundColor: GlobalStyles.colors.errorLight
+        backgroundColor: GlobalStyles.colors.errorLight,
+        borderWidth: 2,
+        borderColor: GlobalStyles.colors.error
     },
-    errorText: {
+    errorTitle: {
         color: GlobalStyles.colors.error,
         textAlign: 'center',
-        paddingVertical: 5
+        paddingVertical: 5,
+        fontWeight: '900'
+    },
+    errorBody: {
+        color: GlobalStyles.colors.error,
+        textAlign: 'center',
+        paddingVertical: 5,
+        fontWeight: '700'
     },
     buttonBox: {
         marginVertical: 20
